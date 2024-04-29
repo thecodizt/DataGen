eaac30fb93f426ec2b424c4479a1aee1848e31bd
Dev <dev@thecodizt.com>
2024-04-29 17:22:16 +0530

    FIX: Dataset bug

--- llm_relation/evaluate.py
+++ llm_relation/evaluate.py
@@ -12,7 +12,7 @@ def evaulate_model(dataset_1, dataset_2, model, context):
         for rightId in range(leftId, len(dataset_2)):
             if leftId != rightId:
 
-                (result, response) = evaluate_record(dataset=dataset_1, dataset_2=dataset_2, model=f'{model}', leftId=leftId, rightId=rightId, context=context)
+                (result, response) = evaluate_record(dataset_1=dataset_1, dataset_2=dataset_2, model=f'{model}', leftId=leftId, rightId=rightId, context=context)
 
                 results_df = pd.concat([results_df, [{"leftId": leftId, "rightId": rightId, "result": result}]], ignore_index=True)
 
